8bd3c0de5684d319eae32c5109cb04f2cc4ce9c3
Tubsmcgee <admhartman@gmail.com>
2018-02-02 21:59:25 -0600

    pengsies and the playsies in the snowsies

--- js/main.js
+++ js/main.js
@@ -1,27 +1,99 @@
 'use strict';
 
-const canvasWidth = 800;
-const canvasHeight = 400;
+class Renderer {
+  constructor({
+    canvas,
+    width,
+    height,
+    playerWidth,
+    playerHeight,
+    penguinWidth,
+    penguinHeight,
+    backgroundImage
+  }) {
+    this.canvas = canvas
+      .attr({width, height})
+      .css({backgroundImage: `url(${backgroundImage})`});
+    this.width = width;
+    this.height = height;
+    this.playerWidth = playerWidth;
+    this.playerHeight = playerHeight;
+    this.penguinWidth = penguinWidth;
+    this.penguinHeight = penguinHeight;
+  }
+  render(penguins, player) {
+    const {
+      width,
+      height,
+      canvas,
+      playerWidth,
+      playerHeight,
+      penguinWidth,
+      penguinHeight
+    } = this;
+    const ctx = canvas[0].getContext('2d');
+    if (player.px !== player.x || player.py !== player.y) {
+      canvas.css({'background-position': `${-player.x}px ${-player.y}px`});
+    }
+    ctx.clearRect(0, 0, width, height);
+    ctx.save();
+    ctx.translate(width / 2 - player.x, height / 2 - player.y);
+    ctx.fillRect(
+      -playerWidth / 2 + player.x,
+      -playerHeight / 2 + player.y,
+      playerWidth,
+      playerHeight
+    );
+    penguins.forEach(penguin => {
+      ctx.save();
+      ctx.translate(penguin.x, penguin.y);
+      ctx.fillStyle = 'black';
+      ctx.fillRect(
+        -penguinWidth / 2,
+        -penguinHeight / 2,
+        penguinWidth,
+        penguinHeight
+      );
+      ctx.fillStyle = 'orange';
+      ctx.fillRect(
+        penguinWidth / 2 * Math.cos(penguin.langle) - 2.5,
+        penguinWidth / 2 * (Math.sin(penguin.langle) + 1) -
+          2.5 -
+          penguinHeight / 2,
+        5,
+        5
+      );
+      ctx.restore();
+    });
+    ctx.restore();
+  }
+}
+
 const playerWidth = 50;
 const playerHeight = 80;
 const numPengs = 5;
 const penguinWidth = 40;
 const penguinHeight = 60;
+
 const movementTypes = [
   (penguin, frame) => {
     const cycle = (frame / 800) % 1;
     if (cycle < 0.4) {
       penguin.x--;
+      penguin.langle = Math.PI;
     } else if (cycle > 0.5 && cycle < 0.9) {
       penguin.x++;
+      penguin.langle = 0;
     }
   },
   (penguin, frame) => {
     const cycle = (frame / 800) % 1;
     if (cycle < 0.4) {
       penguin.y--;
+      penguin.langle = Math.PI * 3 / 2;
     } else if (cycle > 0.5 && cycle < 0.9) {
       penguin.y++;
+      penguin.langle = Math.PI / 2;
     }
   }
 ];
@@ -40,11 +112,17 @@ for (let i = 0; i < numPengs; i++) {
   };
 }
 
-const canvas = $('#canvas')
-  .attr({width: canvasWidth, height: canvasHeight})
-  .css({backgroundImage: 'url(img/snow.jpg)'});
+const renderer = new Renderer({
+  canvas: $('#canvas'),
+  width: 800,
+  height: 400,
+  playerWidth,
+  playerHeight,
+  penguinWidth,
+  penguinHeight,
+  backgroundImage: 'img/snow.jpg'
+});
 
-const ctx = canvas[0].getContext('2d');
 const loop = () => {
   player.px = player.x;
   player.py = player.y;
@@ -54,27 +132,8 @@ const loop = () => {
   if (pressing.w || pressing.ArrowUp) player.y--;
   penguins.forEach(penguin => movementTypes[penguin.type](penguin, frame));
 
-  if (player.px !== player.x || player.py !== player.y) {
-    canvas.css({'background-position': `${-player.x}px ${-player.y}px`});
-  }
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-  ctx.save();
-  ctx.translate(canvasWidth / 2 - player.x, canvasHeight / 2 - player.y);
-  ctx.fillRect(
-    -playerWidth / 2 + player.x,
-    -playerHeight / 2 + player.y,
-    playerWidth,
-    playerHeight
-  );
-  penguins.forEach(penguin =>
-    ctx.fillRect(
-      -penguinWidth / 2 + penguin.x,
-      -penguinHeight / 2 + penguin.y,
-      penguinWidth,
-      penguinHeight
-    )
-  );
-  ctx.restore();
+  renderer.render(penguins, player);
+
   frame++;
   requestAnimationFrame(loop);
 };
